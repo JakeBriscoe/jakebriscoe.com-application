2dd6d61387c0a5e5ac7c3e19416dccc4a98d205d
Jake Briscoe <jake.briscoe@healthnow.co.nz>
2023-03-24 16:55:26 +0000

    ignore tags

--- packages/content-service/main.go
+++ packages/content-service/main.go
@@ -23,7 +23,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
         log.Print("Hello world received a request.")
         target := os.Getenv("TARGET")
         if target == "" {
-                target = "World! :c"
+                target = "World! c:"
         }
         fmt.Fprintf(w, "Hello %s!\n", target)
 }
