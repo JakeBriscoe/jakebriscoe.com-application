e63dba7020c83f7b82ac794e522b8525977f14c4
Jake Briscoe <jake.briscoe@healthnow.co.nz>
2023-04-26 16:49:43 +0100

    init script to create db tables and users

---
+++ deployment/postgres/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Define array of service names
+services=("game" "content" "leaderboard" "user")
+
+# Loop through services and create secrets and databases
+for service in "${services[@]}"; do
+    # Generate random username and password
+    username=$(openssl rand -hex 6)
+    password=$(openssl rand -hex 16)
+
+    # Create Kubernetes Secret
+    kubectl create secret generic ${service}-db-credentials \
+    --from-literal=username=$username \
+    --from-literal=password=$password
+
+    # Create the user and database
+    psql -U postgres -c "CREATE USER $username WITH PASSWORD '$password';"
+    psql -U postgres -c "CREATE DATABASE ${service}_db OWNER $username;"
+done
