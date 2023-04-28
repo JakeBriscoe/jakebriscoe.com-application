a752f16df21e87cf5340abba3ffee5e99078ba10
Jake Briscoe <jake.briscoe@healthnow.co.nz>
2023-04-28 20:21:38 +0100

    use sh instead of bash

--- deployment/postgres/startup.sh
+++ deployment/postgres/startup.sh
@@ -15,6 +15,8 @@ for service in "${services[@]}"; do
     --from-literal=username=$username \
     --from-literal=password=$password
 
+    # Can also write these tp docker-entrypoint-initdb.d/01-create-dbs.sh 
+    # which will auto run on init
     # Create the user and database
     psql -U postgres -c "CREATE USER $username WITH PASSWORD '$password';"
     psql -U postgres -c "CREATE DATABASE ${service}_db OWNER $username;"
