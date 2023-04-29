7ddad4ca2f352c0b5c56bf2b1c84bbeca3fd9f74
Jake Briscoe <jake.briscoe@healthnow.co.nz>
2023-04-29 09:51:33 +0100

    Fixed syntax error in database setup script

--- deployment/postgres/startup.sh
+++ deployment/postgres/startup.sh
@@ -29,8 +29,8 @@ for service in $SERVICES; do
     echo "Connecting to server with username ${POSTGRES_USER} and password ${PGPASSWORD} and db ${POSTGRES_DB}"
 
     # Create the user and database
-    psql -h postgres-service -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $username WITH PASSWORD '$password';"
-    psql -h postgres-service -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE DATABASE ${service}_db OWNER $username;"
+    psql -h postgres-service -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER '$username' WITH PASSWORD '$password';"
+    psql -h postgres-service -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE DATABASE '${service}_db' OWNER '$username';"
 
     echo "Database created for ${service}"
 done
