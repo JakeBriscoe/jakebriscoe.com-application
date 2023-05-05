f30bf98c1bfe61c93e8fd1689bfb6f69c3932b93
Jake Briscoe <jake.briscoe@healthnow.co.nz>
2023-05-05 14:53:05 +0100

    add more logging

--- packages/content-service/internal/api/server.go
+++ packages/content-service/internal/api/server.go
@@ -6,14 +6,15 @@ import (
 )
 
 func StartServer() {
-	log.Print("Starting server")
+	log.Print("Setting server handlers")
 	// Register HTTP request handlers
 	http.HandleFunc("/tracks", FetchTracks)
 
+	log.Print("Starting server")
 	// Start the server
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal("Server failed to start: ", err)
 	}
 	log.Print("Server started on port 8080")
 }
